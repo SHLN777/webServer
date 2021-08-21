a549f1c623f7b02e6dc6e6855cb104bbd0167369
SHLN777 <18339906771@163.com>
2021-08-21 20:40:57 +0800

    threadpool

---
+++ Threadpool/threadpool.h
@@ -0,0 +1,153 @@
+#ifndef THREADPOOL_H
+#define THREADPOOL_H
+
+#include <list>
+#include <cstdio>
+#include <exception>
+#include <pthread.h>
+#include "../Lock/locker.h"
+#include "../MySQL/Mysql_pool.h"
+
+template <typename T>
+class threadpool
+{
+public:
+    /*thread_number是线程池中线程的数量，max_requests是请求队列中最多允许的、等待处理的请求的数量*/
+    threadpool(int actor_model, connection_pool *connPool, int thread_number = 8, int max_request = 10000);
+    ~threadpool();
+    bool append(T *request, int state);
+    bool append_p(T *request);
+
+private:
+    
+    static void *worker(void *arg); //工作线程调用函数
+    void run();
+
+private:
+    int m_thread_number;        //线程池的线程数
+    int m_max_requests;         //请求队列最大请求数
+    pthread_t *m_threads;       //描述线程池的数组，其大小为m_thread_number
+    std::list<T *> m_workqueue; //任务链表
+    locker m_queuelocker;       //任务链表的互斥锁
+    sem m_queuestat;            //信号量，表示任务链表中是否有需要完成的任务
+    connection_pool *m_connPool;  //数据库连接
+    int m_actor_model;          //Reactor:1, proactor:0
+};
+
+template <typename T>
+threadpool<T>::threadpool( int actor_model, connection_pool *connPool, int thread_number, int max_requests) : m_actor_model(actor_model),m_thread_number(thread_number), m_max_requests(max_requests), m_threads(NULL),m_connPool(connPool)
+{
+    if (thread_number <= 0 || max_requests <= 0)
+        throw std::exception();
+    m_threads = new pthread_t[m_thread_number];
+    if (!m_threads)
+        throw std::exception();
+    for (int i = 0; i < thread_number; ++i)
+    {
+        if (pthread_create(m_threads + i, NULL, worker, this) != 0)
+        {
+            delete[] m_threads;
+            throw std::exception();
+        }
+        if (pthread_detach(m_threads[i]))
+        {
+            delete[] m_threads;
+            throw std::exception();
+        }
+    }
+}
+template <typename T>
+threadpool<T>::~threadpool()
+{
+    delete[] m_threads;
+}
+template <typename T>
+bool threadpool<T>::append(T *request, int state)   //state指示读写
+{
+    m_queuelocker.lock();
+    if (m_workqueue.size() >= m_max_requests)
+    {
+        m_queuelocker.unlock();
+        return false;
+    }
+    request->m_state = state;
+    m_workqueue.push_back(request);
+    m_queuelocker.unlock();
+    m_queuestat.post();
+    return true;
+}
+template <typename T>
+bool threadpool<T>::append_p(T *request)   //Proactor调用
+{
+    m_queuelocker.lock();
+    if (m_workqueue.size() >= m_max_requests)
+    {
+        m_queuelocker.unlock();
+        return false;
+    }
+    m_workqueue.push_back(request);
+    m_queuelocker.unlock();
+    m_queuestat.post();
+    return true;
+}
+template <typename T>
+void *threadpool<T>::worker(void *arg)
+{
+    threadpool *pool = (threadpool *)arg;
+    pool->run();
+    return pool;
+}
+template <typename T>
+void threadpool<T>::run()     //工作线程既读取数据，又发送数据
+{
+    while (true)
+    {
+        m_queuestat.wait();
+        m_queuelocker.lock();
+        if (m_workqueue.empty())
+        {
+            m_queuelocker.unlock();
+            continue;
+        }
+        T *request = m_workqueue.front();
+        m_workqueue.pop_front();
+        m_queuelocker.unlock();
+        if (!request)
+            continue;
+        if (1 == m_actor_model)       //Reactor
+        {
+            if (0 == request->m_state)    //读
+            {
+                if (request->read_once())
+                {
+                    request->improv = 1;
+                    connectionRAII mysqlcon(&request->mysql, m_connPool);
+                    request->process();
+                }
+                else
+                {
+                    request->improv = 1;
+                    request->timer_flag = 1;
+                }
+            }
+            else
+            {
+                if (request->write())   //写
+                {
+                    request->improv = 1;
+                }
+                else
+                {
+                    request->improv = 1;
+                    request->timer_flag = 1;
+                }
+            }
+        }
+        else                      //Proactor，  只在读数据时使用
+        {
+            connectionRAII mysqlcon(&request->mysql, m_connPool);
+            request->process();      //分析缓冲区的数据，然后将数据写入缓冲区，等待发送
+        }
+    }
+}
+#endif
